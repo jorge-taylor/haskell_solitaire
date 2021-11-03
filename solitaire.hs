fbb21933b09bb2ae3be416aaa4bfd9d0d0702ee4
Jorge <jorgetaylor@protonmail.com>
2021-11-03 23:07:59 +0000

    Added eight off board basic functionality

--- solitaire.hs
+++ solitaire.hs
@@ -1,44 +1,64 @@
 import System.Random
 import Data.List
 
--- DEFINE INITIAL DATATYPES
-data Suit = Hearts | Clubs | Spades | Diamonds deriving (Eq, Ord, Enum, Show)
-data Pip = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King deriving (Eq, Ord, Enum, Show)
+-- Initial datatypes
+data Suit = Hearts | Clubs | Spades | Diamonds
+  deriving (Eq, Ord, Enum, Show)
+data Pip = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King
+  deriving (Eq, Ord, Enum, Show)
 type Card = (Suit, Pip)
 type Deck = [Card]
 
--- IMPLEMENT BASIC FUNCTIONALITY
+-- Datatypes for Eight Off Board
+type Foundations = [Deck]
+type Columns = [Deck]
+type Reserves = [Card]
+data Board = Empty | EOBoard deriving (Eq, Ord, Enum, Show)
+type EOBoard = (Foundations, Columns, Reserves)
+{--
+instance Show EOBoard where
+  Foundations = "Foundations" + Foundations
+  Columns = "Columns" + Columns
+  Reserves = "Reserves" + Reserves
+--}
+
 -- List all 52 cards in a pack
 pack :: Deck
 pack = [(suit, pip) | suit <- [Hearts .. Diamonds], pip <- [Ace .. King]]
 
 -- Returns the successor of a card (successor of a king is an ace of the same suit)
 sCard :: Card -> Card
-sCard (s, King) = (s, Ace) 
-sCard (s, p) = (s, succ p)
+sCard (suit, King) = (suit, Ace) 
+sCard (suit, pip) = (suit, succ pip)
 
 -- Returns the successor of a card (predecessor of an ace is a king of the same suit)
 pCard :: Card -> Card
-pCard (s, Ace) = (s, King)
-pCard (s, p) = (s, pred p)
+pCard (suit, Ace) = (suit, King)
+pCard (suit, pip) = (suit, pred pip)
 
 -- Checks if the given card is an ace
 isAce :: Card -> Bool
---isAce (s, p) | p == Ace = True
---             | otherwise = False
-isAce (s, p) = if p == Ace then True else False
+isAce (_, pip) = pip == Ace
 
 -- Checks if the given card is a king
 isKing :: Card -> Bool
---isKing(s, p) | p == King = True
---             | otherwise = False
-isKing (s, p) = if p == King then True else False
+isKing(_, pip) = pip == King
 
 -- Takes a deck and returns a shuffled deck of the same cards
-shuffle :: Int -> Deck -> Deck
+shuffle :: Int -> Deck
 cmp (x1,y1) (x2,y2) = compare y1 y2
-shuffle n d = [x | (x, n) <- sortBy cmp (zip d ((randoms (mkStdGen n)) :: [Int]))]
+shuffle seed = [x | (x, n) <- sortBy cmp (zip pack ((randoms (mkStdGen seed)) :: [Int]))]
 
--- DEFINE DATATYPES TO REPRESENT AN EIGHT-OFF BOARD
+-- Deals an eight off board given a seed 
+eoDeal :: Int -> EOBoard
+eoDeal seed = (foundations, columns, reserves)
+  where deck = shuffle seed
+        foundations = []
+        reserves = take 4 deck
+        columns = splitDeck (drop 4 deck)
 
-
+-- Splits a deck recursively into piles of 6, to create 8 columns 
+splitDeck :: Deck -> [Deck]
+splitDeck [] = []
+splitDeck deck = head : splitDeck tail
+    where (head, tail) = splitAt 6 deck
