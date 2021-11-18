25c55bb179e93163225cbd039cdc35b1c5e6416d
jorge-taylor <jorgetaylor@protonmail.com>
2021-11-18 15:00:14 +0000

    Implemented show function, updated data Board label to include EOBoard and removed the 'type EOBoard'

--- solitaire.hs
+++ solitaire.hs
@@ -11,36 +11,24 @@ import System.Random
 -- Initial datatypes
 data Suit = Hearts | Clubs | Spades | Diamonds
   deriving (Eq, Ord, Enum, Show)
-
 data Pip = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King
   deriving (Eq, Ord, Enum, Show)
-
 type Card = (Suit, Pip)
-
 type Deck = [Card]
 
 -- Datatypes for Eight Off Board
 type Foundations = [Deck]
-
 type Columns = [Deck]
-
 type Reserves = [Card]
+data Board = Empty | EOBoard (Foundations, Columns, Reserves)
+  deriving (Eq, Ord)
+--type EOBoard = (Foundations, Columns, Reserves)
 
-data Board = Empty | EOBoard
-  deriving (Eq, Ord, Enum, Show)
-
-type EOBoard = (Foundations, Columns, Reserves)
-
--- output an EOBoard in the desired format
--- problem: this is not the correct way to implement this, what about: "instance Show EOBoard where"?
-outputEO :: EOBoard -> IO ()
-outputEO board@(f, c, r) = do
-  putStr "EOBoard\nFoundations "
-  putStrLn (show f)
-  putStr "Columns\n"
-  putStrLn (show c)
-  putStr "Reserve     "
-  putStrLn (show r)
+instance Show Board where
+  show (EOBoard (f, c, r)) = "EOBoard\nFoundations  " ++ show f ++ "\nColumns\n" ++ (showColumns c) ++ "Reserves    " ++ show r
+    where
+      showColumns [] = ""
+      showColumns (c:cs) = show c ++ "\n" ++ showColumns cs 
 
 -- List all 52 cards in a pack
 pack :: Deck
@@ -66,20 +54,19 @@ isKing (_, pip) = pip == King
 
 -- Takes a deck and returns a shuffled deck of the same cards
 shuffle :: Int -> Deck
-
 cmp (x1, y1) (x2, y2) = compare y1 y2
-
 shuffle seed = [x | (x, n) <- sortBy cmp (zip pack (randoms (mkStdGen seed) :: [Int]))]
 
 -- Removes the first instance of a card from a given list of cards (deck)
 remove :: Card -> Deck -> Deck
+remove card [] = []
 remove card (element : deck)
   | card == element = deck
   | otherwise = element : remove card deck
 
 -- Deals an eight off board given a seed
-eoDeal :: Int -> EOBoard
-eoDeal seed = (foundations, columns, reserves)
+eoDeal :: Int -> Board
+eoDeal seed = EOBoard (foundations, columns, reserves)
   where
     deck = shuffle seed
     foundations = []
@@ -94,44 +81,40 @@ splitColumns deck = head : splitColumns tail
     (head, tail) = splitAt 6 deck
 
 -- Calls recursively autoplay while there is a valid move to foundations
-toFoundations :: EOBoard -> EOBoard
+toFoundations :: Board -> Board
 toFoundations board
   | anyValidMoves board = toFoundations (autoplay board)
   | otherwise = board
 
 -- Check that there is a valid move given an eight off board
-anyValidMoves :: EOBoard -> Bool
+anyValidMoves :: Board -> Bool
 anyValidMoves board = (not . null) (getValidAces board ++ getValidSuccessors board)
 
 -- Moves all valid cards to foundations (copying them over to foundations and deleting them from their original location)
-autoplay :: EOBoard -> EOBoard
-autoplay board@(foundations, columns, reserves) = (updated_foundations, updated_columns, updated_reserves)
+autoplay :: Board -> Board
+autoplay (EOBoard (foundations, columns, reserves)) = EOBoard (updated_foundations, updated_columns, updated_reserves)
   where
-    moveable_aces = getValidAces board
-    moveable_successors = getValidSuccessors board
-    --possibles_in_columns =
-    --possibles_in_reserves =
+    moveable_aces = getValidAces (EOBoard (foundations, columns, reserves))
+    moveable_successors = getValidSuccessors (EOBoard (foundations, columns, reserves))
 
-    (updated_foundations, _, _) = foldr moveValidsToFoundations board (moveable_aces ++ moveable_successors)
-    updated_columns = filter (not . null) (map (\e -> if isAce (last e) || any (elem (last e)) [moveable_successors] then init e else e) columns)
-    --updated_columns = [element | element <- possibles_in_columns]
-    updated_reserves = filter (\e -> not (isAce e || e `elem` moveable_successors)) reserves
+    EOBoard (updated_foundations, updated_columns, updated_reserves) = foldr moveValidToFoundations (EOBoard (foundations, columns, reserves)) (moveable_aces ++ moveable_successors)
+    --updated_columns = filter (not . null) (map (\e -> if isAce (last e) || any (elem (last e)) [moveable_successors] then init e else e) columns)
+    --updated_reserves = filter (\e -> not (isAce e || e `elem` moveable_successors)) reserves
 
 -- Get a list of all aces which are in the topmost columns or reserves which can be moved to foundations
-getValidAces :: EOBoard -> Deck
-getValidAces (_, columns, reserves) = filter isAce (map last columns ++ reserves)
+getValidAces :: Board -> Deck
+getValidAces (EOBoard (_, columns, reserves)) = filter isAce (map last columns ++ reserves)
 
 -- Get a list of all successors which are in the topmost columns or reserves which can be moved to foundations
-getValidSuccessors :: EOBoard -> Deck
---filter (\e -> e `elem` (map head (filter (not.null) columns) ++ reserves)) (map (\e -> if isKing (head e) then head e else (sCard.head) e) foundations)
-getValidSuccessors (foundations, columns, reserves) = [c2 | c1 <- topmost_foundations, c2 <- topmost_possibles, sCard c1 == c2 && not (isKing c1)]
+getValidSuccessors :: Board -> Deck
+getValidSuccessors (EOBoard (foundations, columns, reserves)) = [c2 | c1 <- topmost_foundations, c2 <- topmost_possibles, sCard c1 == c2 && not (isKing c1)]
   where
     topmost_foundations = map last (filter (not . null) foundations)
     topmost_possibles = map last (filter (not . null) columns) ++ reserves
 
 -- Moves valid cards to foundations
-moveValidsToFoundations :: Card -> EOBoard -> EOBoard
-moveValidsToFoundations card (foundations, columns, reserves) = (updated_foundations, updated_columns, updated_reserves)
+moveValidToFoundations :: Card -> Board -> Board
+moveValidToFoundations card (EOBoard (foundations, columns, reserves)) = EOBoard (updated_foundations, updated_columns, updated_reserves)
   where
     updated_foundations = cardToFoundations card foundations
     updated_columns = map (remove card) columns
